99b34cbe1e507e759824c546d91a242dbd9ae8b7
Gislene Pereira <gislene01@gmail.com>
2015-11-03 19:43:44 -0300

    [WIP] Adjusting link colors and improving post data formatting

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -22,7 +22,7 @@ $(window).load(function() {
     }
     var imgElement = $('<img>').attr('src', post.media_url);
     var textElement = $('<p>').html(post.text).attr('class', 'text');
-    var authorElement = $('<a>').text(post.screen_name).attr('href', post.profile_image_url).attr('class', 'author');
+    var authorElement = $('<a>').text("@"+post.screen_name+":").attr('href', post.profile_image_url).attr('class', 'author');
     var divElement = $('<div>').attr('class', 'post-data').append(authorElement).append(textElement);
     var liElement = $('<li>').attr('data-post-id', post.id).append(imgElement).append(divElement);
     dashtagSlider.data('flexslider').addSlide(liElement);
